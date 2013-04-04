26b465ec871cbd0b742df416b7e22778bcc93a4d
RemyD <remyd@civx.us>
2013-04-04 18:00:01 -0400

    Neon line example from pygal.org

---
+++ neonline.py
@@ -0,0 +1,7 @@
+from pygal.style import NeonStyle
+chart = pygal.StackedLine(fill=True, style=NeonStyle)
+chart.add('A', [1, 3,  5, 16, 13, 3,  7])
+chart.add('B', [5, 2,  3,  2,  5, 7, 17])
+chart.add('C', [6, 10, 9,  7,  3, 1,  0])
+chart.add('D', [2,  3, 5,  9, 12, 9,  5])
+chart.add('E', [7,  4, 2,  1,  2, 10, 0])
